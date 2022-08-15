edb3b73b8f9f63d79879093ffd01e37ad769cef5
Berat UNLUCE <beratunluce@hotmail.com>
2022-08-15 20:55:47 +0300

    partials and view folder added

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const ejs = require("ejs");
 const app = express();
-
-const blog = {id: 1, title: "Blog title", description: "Blog description"};
+const path = require("path");
 const port = 4000;
-app.listen(port, () => console.log(`App listening on port ${port}!`));
-app.get("/", (req, res) => {
-	res.send(blog);
+/// template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+
+app.get("/about", (req, res) => {
+	res.render("about");
 });
+
+app.get("/add_post", (req, res) => {
+	res.render("add_post");
+});
+app.get("/index", (req, res) => {
+	res.render("index");
+});
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
