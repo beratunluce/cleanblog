077f930cfbb0ab67a969a1118893a84432eb4211
Berat UNLUCE <beratunluce@hotmail.com>
2022-09-05 19:52:45 +0300

    new

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
+const mongoose = require("mongoose");
 const path = require("path");
 const port = 4000;
+const Post = require("./models/Post");
+
+///database connection
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+///middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 /// template engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public"));
 
 app.get("/about", (req, res) => {
 	res.render("about");
@@ -15,12 +28,21 @@ app.get("/about", (req, res) => {
 app.get("/add_post", (req, res) => {
 	res.render("add_post");
 });
-app.get("/index", (req, res) => {
-	res.render("index");
+app.get("/index", async (req, res) => {
+	const posts = await Post.find({});
+	res.render("index", {
+		posts,
+	});
 });
 
 app.get("/", (req, res) => {
-	res.render("index");
+	res.redirect("/index");
+});
+
+app.post("/posts", async (req, res) => {
+	await Post.create(req.body);
+
+	res.redirect("/index");
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
